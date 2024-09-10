2df614d4eef7f522c839acc664b57d48fd472395
федор соснин <sosninfedor@gmail.com>
2024-09-10 23:08:17 +0500

    Task1

---
+++ Tyuiu.SosninFM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SosninFM.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "Фёдор";
+            int age = 19;
+            Console.WriteLine("Здравствуйте, меня зовут "+name+", мне "+age+" лет!");
+            Console.ReadKey();
+
+        }
+    }
+}
