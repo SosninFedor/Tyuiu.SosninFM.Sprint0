6e3d32c5c237839bad30a4947b7921ff8bdbd7c4
федор соснин <sosninfedor@gmail.com>
2024-09-12 01:34:06 +0500

    Task3

---
+++ Tyuiu.SosninFM.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SosninFM.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+
+
+        }
+
+
+    }
+}
---
+++ Tyuiu.SosninFM.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SosninFM.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.SosninFM.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+
+
+        }
+    }
+}
