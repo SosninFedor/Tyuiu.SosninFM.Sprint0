bc81074f7e03fc9211f0f84a90e88292558b4870
федор соснин <sosninfedor@gmail.com>
2024-09-17 14:07:22 +0500

    Task6

---
+++ Tyuiu.SosninFM.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SosninFM.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SosninFM.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4513b256-597d-4286-bb57-b94db1609b04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SosninFM.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SosninFM.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.SosninFM.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arraynums = new int[] { 1, 2, 3, 4, 5};
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(Arraynums));
+
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(Arraynums));
+
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(Arraynums));
+
+            Console.ReadKey();
+        }
+    }
+}
